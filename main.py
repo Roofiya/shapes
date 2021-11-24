9e3f3f011e4d8c7805bbe3bf0535483160a5c87f
Roofiya <roofiyak@gmail.com>
2021-11-24 11:27:47 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import sys
+import os
+from math import pi
 
 
-# Press the green button in the gutter to run the script.
+class Circle:
+    def __init__(self, radius, fill='red', stroke='black'):
+        self._radius = radius #private/protected
+        self._fill = fill
+        self._stroke = stroke
+
+    def calculate_area(self):
+        '''Calculate area'''
+        return pi * self._radius ** 2
+
+    @property # decorator # Public access for radius - read only
+    def rad(self):
+        return self._radius # Reflecting private radius publicly
+
+    def __len__(self):
+        return int(2 * pi *self._radius)
+
+    def __call__(self):
+        return "I am a circle!"
+
+def main():
+    circle = Circle(5.0, fill='orange', stroke='red')
+    print(f"area = {circle.calculate_area()}")
+    #print(circle._radius)
+    #circle.radius = 3
+    print(circle._radius)# Violating private rule, so use @property
+    #print(f"ra = {circle.rad()}")
+    print(f"circumference is {len(circle)}")
+    print(circle())
+    return 0
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sys.exit(main())
